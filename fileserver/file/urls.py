14572df333fb168e854fc2ccc80c7cd6253d0929
Tito-Curtis <jnrcurtis75@gmail.com>
2024-05-23 19:36:58 +0000

    worked on user feed page, filter and search functionalities

--- fileserver/file/urls.py
+++ fileserver/file/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('feed/',views.feed_page_view,name='feed_page'),
     path('download/<int:document_id>',views.download_document_view,name='download_document'),
     path('send/<int:document_id>',views.send_email_view,name='send_email'),
-    path('view/<int:document_id>',views.view_documents,name='view_documents')
+    path('view/<int:document_id>',views.view_documents,name='view_documents'),
+    path('filter/',views.filter_category_view,name='filter_document'),
+    path('search/',views.search_document_view,name='search'),
 ]
