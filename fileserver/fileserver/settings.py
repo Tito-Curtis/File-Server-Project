cd976555a201e49dfbee736dce42a09ee5df2826
Tito-Curtis <jnrcurtis75@gmail.com>
2024-05-27 20:36:10 +0000

    added staticfiles and prepared project for deployment

--- fileserver/fileserver/settings.py
+++ fileserver/fileserver/settings.py
@@ -128,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS =[
+    BASE_DIR/'file'/'static',
+]
+print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
